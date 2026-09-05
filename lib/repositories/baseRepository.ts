635d4c7dee257900659a103de69c30cda6746024
MD-Multiservices <mehdi.derghal@gmail.com>
2026-09-05 14:02:35 +0200

    Deprecate local repository persistence

--- lib/repositories/baseRepository.ts
+++ lib/repositories/baseRepository.ts
@@ -1,37 +1,12 @@
-import { lire, enregistrer } from "@/lib/database";
+import { enregistrer as ecrireCompat, lire as lireCompat } from "@/lib/database";
 
+/** @deprecated Conservé uniquement pour les anciens services non branchés à l'interface. */
 export class BaseRepository<T extends { id: string }> {
-  constructor(private table: Parameters<typeof lire>[0]) {}
-
-  getAll(): T[] {
-    return lire<T>(this.table);
-  }
-
-  getById(id: string): T | undefined {
-    return this.getAll().find((item) => item.id === id);
-  }
-
-  saveAll(items: T[]) {
-    enregistrer(this.table, items);
-  }
-
-  add(item: T) {
-    const items = this.getAll();
-    items.push(item);
-    this.saveAll(items);
-  }
-
-  update(item: T) {
-    const items = this.getAll().map((i) =>
-      i.id === item.id ? item : i
-    );
-
-    this.saveAll(items);
-  }
-
-  delete(id: string) {
-    this.saveAll(
-      this.getAll().filter((i) => i.id !== id)
-    );
-  }
+  constructor(private table: Parameters<typeof lireCompat>[0]) {}
+  getAll(): T[] { return lireCompat<T>(this.table); }
+  getById(id: string): T | undefined { return this.getAll().find((item) => item.id === id); }
+  saveAll(items: T[]): void { ecrireCompat(this.table, items); }
+  add(item: T): void { this.saveAll([...this.getAll(), item]); }
+  update(item: T): void { this.saveAll(this.getAll().map((i) => i.id === item.id ? item : i)); }
+  delete(id: string): void { this.saveAll(this.getAll().filter((i) => i.id !== id)); }
 }
